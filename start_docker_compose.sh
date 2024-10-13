24e772dec7efa0b75b10523c072db328afe89a76
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-13 16:11:51 +0200

    updating submodules

--- start_docker_compose.sh
+++ start_docker_compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop and remove all containers, networks, and volumes
-# docker-compose down -v
+docker-compose down -v
 
 # Build the Docker Compose services without using cache
 docker-compose build --no-cache
