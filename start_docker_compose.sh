8d6b34145695e75f2cf31a1c6f09f39eace48f0d
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-13 09:50:38 +0200

    fix cache issue with docker

--- start_docker_compose.sh
+++ start_docker_compose.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# build the docker compose
-docker-compose build
+# Stop and remove all containers, networks, and volumes
+# docker-compose down -v
+
+# Build the Docker Compose services without using cache
+docker-compose build --no-cache
 
 # Start Docker Compose
 docker-compose up
