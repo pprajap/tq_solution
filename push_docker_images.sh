68859efb08a66fd8c662043dbc859627de22cfe0
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-14 12:17:19 +0200

    helper script and readme

---
+++ push_docker_images.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Treat unset variables as an error
+set -u
+
+# call ./build_docker_images.sh to build all images
+./build_docker_images.sh
+
+# Docker login
+if [ -z "${DOCKER_USERNAME:-}" ] || [ -z "${DOCKER_PASSWORD:-}" ]; then
+  echo "DOCKER_USERNAME and DOCKER_PASSWORD must be set"
+  exit 1
+fi
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+
+# push all above to docker hub
+docker tag tq_backend:latest  $DOCKER_USERNAME/tq_backend:latest
+docker push $DOCKER_USERNAME/tq_backend:latest
+
+docker tag tq_frontend_desktop:latest  $DOCKER_USERNAME/tq_frontend_desktop:latest
+docker push $DOCKER_USERNAME/tq_frontend_desktop:latest
+
+docker tag tq_frontend_web_light:latest  $DOCKER_USERNAME/tq_frontend_web_light:latest
+docker push $DOCKER_USERNAME/tq_frontend_web_light:latest
+
+docker tag tq_frontend_web:latest  $DOCKER_USERNAME/tq_frontend_web:latest
+docker push $DOCKER_USERNAME/tq_frontend_web:latest
+
