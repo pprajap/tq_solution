78f99957f190444f4be7131d544b2909abfcbda5
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-15 20:54:23 +0200

    update submodules & tq_sdk project

---
+++ tq_sdk/terra_quantum_sdk.cpp
@@ -0,0 +1,24 @@
+#include "terra_quantum_sdk.h"
+#include <iostream>
+
+namespace terra_quantum {
+
+TerraQuantumSDK::TerraQuantumSDK() {
+    // Constructor implementation
+}
+
+TerraQuantumSDK::~TerraQuantumSDK() {
+    // Destructor implementation
+}
+
+void TerraQuantumSDK::initialize() {
+    std::cout << "Initializing Terra Quantum SDK..." << std::endl;
+    // Initialization code
+}
+
+void TerraQuantumSDK::finalize() {
+    std::cout << "Finalizing Terra Quantum SDK..." << std::endl;
+    // Finalization code
+}
+
+} // namespace terra_quantum
