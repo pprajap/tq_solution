6d6534397cdbc15046b80cbc3c19c4b7cf5e0788
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-15 09:18:09 +0200

    update files

--- README.md
+++ README.md
@@ -1,31 +1,12 @@
 # tq_solution
 
-# Start with a Clean Slate
-
-**Warning:** This will remove all Docker containers and images.
-```sh
-./cleanup.sh
-```
 
 # Build All Docker Images
-
-This script will internally call `cleanup.sh` before starting.
 ```sh
 ./build_docker_images.sh
 ```
 
-# Upload/Push Docker Images to Docker Hub
 
-Set your Docker Hub credentials:
-```sh
-export DOCKER_USERNAME=<your_username>
-export DOCKER_PASSWORD=<your_password>
-```
-
-This script will internally call `build_docker_images.sh` before starting.
-```sh
-./push_docker_images.sh
-```
 
 # Start Desktop/Web Solution
 
---
+++ push_docker_images.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Treat unset variables as an error
+set -u
+
+echo "Building all images..."
+./build_docker_images.sh
+echo "Images build completed."
+
+# Docker login
+docker login
+echo "Docker login successful."
+
+# fetch user name from docker login
+export DOCKER_USERNAME=$(docker info | grep Username | awk '{print $2}')
+
+# push all above to docker hub
+docker tag tq-backend:latest  $DOCKER_USERNAME/tq-backend:latest
+docker push $DOCKER_USERNAME/tq-backend:latest
+
+docker tag tq-frontend-desktop:latest  $DOCKER_USERNAME/tq-frontend-desktop:latest
+docker push $DOCKER_USERNAME/tq-frontend-desktop:latest
+
+docker tag tq-frontend-web-light:latest  $DOCKER_USERNAME/tq-frontend-web-light:latest
+docker push $DOCKER_USERNAME/tq-frontend-web-light:latest
+
+docker tag tq-frontend-web:latest  $DOCKER_USERNAME/tq-frontend-web:latest
+docker push $DOCKER_USERNAME/tq-frontend-web:latest
+
