b8864cd97d944d0693121e7c52b3521aa2293d14
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-16 20:07:45 +0200

    code formating

--- reverse-proxy/build_reverse_proxy_docker_image.sh
+++ reverse-proxy/build_reverse_proxy_docker_image.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Building tq-reverse-proxy image..."
 # build tq-reverse-proxy image
 docker build -f Dockerfile -t tq-reverse-proxy .
-echo "Image build completed."
--- reverse-proxy/push_reverse_proxy_docker_image.sh
+++ reverse-proxy/push_reverse_proxy_docker_image.sh
@@ -6,19 +6,17 @@ set -e
 # Treat unset variables as an error
 set -u
 
-echo "Building tq-reverse-proxy image..."
+# build reverse proxy image
 ./build_reverse_proxy_docker_image.sh
-echo "Image build completed."
 
 # Docker login
 docker login
-echo "Docker login successful."
 
 # fetch user name from docker login
 export DOCKER_USERNAME=$(docker info | grep Username | awk '{print $2}')
 
-echo "Tagging and pushing the image to Docker Hub..."
-# push above to docker hub
+# tag reverse proxy image
 docker tag tq-reverse-proxy:latest  $DOCKER_USERNAME/tq-reverse-proxy:latest
+
+# push reverse proxy image to docker hub
 docker push $DOCKER_USERNAME/tq-reverse-proxy:latest
-echo "Image pushed to Docker Hub successfully."
