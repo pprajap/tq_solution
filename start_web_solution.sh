324cf6cd7b06627a61ad3cbbeaddaf0d6d11d183
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-17 10:10:46 +0200

    files clean up

--- start_web_solution.sh
+++ start_web_solution.sh
@@ -8,3 +8,6 @@ docker-compose -f docker-compose-web.yml build --no-cache
 
 # Start Docker Compose
 docker-compose -f docker-compose-web.yml up
+
+# echo "Frontend service is running. Visit http://localhost:3000/apptq_frontend.html"
+echo "Frontend is running. Visit http://localhost:3000/apptq_frontend.html"
